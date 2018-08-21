dd6e2eaaa1b87c6b10f2b79e4e85fb92176c161a
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:15:12 +0530

    first commit

---
+++ Chapter8Program1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8Program1
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            MessageBox.Show(card.Name);
+        }
+    }
+}
---
+++ Chapter8Program1/Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8Program1
+{
+   public enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+}
