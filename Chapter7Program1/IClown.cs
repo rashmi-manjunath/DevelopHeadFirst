2b9cbb99ae901c0e9037dc0acb1db301bc4f5006
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:05:43 +0530

    first commit

---
+++ Chapter7Program1/IClown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7Program1
+{
+    interface IClown
+    {
+        string FunnyThingsIHave { get; }
+
+        void Honk();
+    }
+}
---
+++ Chapter7Program1/TallGuy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter7Program1
+{
+    class TallGuy:IClown
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            MessageBox.Show("My name is " + Name + " and I'm " + Height + " inches tall");
+        }
+
+       public string FunnyThingsIHave
+        {
+            get
+            {
+                return "Big shoes";
+            }
+        }
+
+        public void Honk()
+        {
+            MessageBox.Show("Honk! Honk!");
+        }
+    }
+}
