354c333b3cd5479edaa689285e3deb1b1db178c2
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 15:34:43 +0530

    first commit

---
+++ Chapter2Program2-IfElse/FunWithIfElse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class FunWithIfElse : Form
+    {
+        public FunWithIfElse()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked==true)
+            {
+                if(label1.BackColor == Color.Red)
+                {
+                    label1.BackColor = Color.Blue;
+                }
+                else
+                {
+                    label1.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Plese check Enable color changing");
+            }
+        }
+
+    }
+}
