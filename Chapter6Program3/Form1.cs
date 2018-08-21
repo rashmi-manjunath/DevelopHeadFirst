f66d88a2c78ab6d34ac268440661af1bf5e23a21
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:02:38 +0530

    first commit

---
+++ Chapter6Program3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter6Program3
+{
+    public partial class Form1 : Form
+    {
+        Queen queen;
+        public Form1()
+        {
+            InitializeComponent();
+            Worker[] workers = new Worker[4];
+            workers[0] = new Worker(new string[] {"Nectar collector","Honey manufacturing"});
+            workers[1] = new Worker(new string[] { "Egg care", "Baby bee tutoring"});
+            workers[2] = new Worker(new string[] { "Hive maintenance", "String patrol"});
+            workers[3] = new Worker(new string[] { "Nectar collector", "Honey manufacturing"});
+            queen = new Queen(workers);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (queen.AssignWork(workerBeeJob.Text, (int)shifts.Value) == false)
+            {
+                MessageBox.Show("No workers are available to do this job '" + workerBeeJob.Text + " '", "The queen bee says..");
+            }
+            else
+                MessageBox.Show("The job '" + workerBeeJob.Text + " ' will be done in " + shifts.Value + " shifts", "the queen bee says..");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            report.Text = queen.WorkNextShift();
+        }
+    }
+}
---
+++ Chapter6Program3/Worker.cs
@@ -0,0 +1,60 @@
+﻿namespace Chapter6Program3
+{
+    class Worker
+    {
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsToWork - shiftsWorked;
+            }
+        }
+        private string currentJob = "";
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+        public bool DoThisJob(string job, int numberOfShifts)
+        {
+            if (!string.IsNullOrEmpty(currentJob))
+                return false;
+            for (int i = 0; i < jobsICanDo.Length; i++)
+                if (jobsICanDo[i] == job)
+                {
+                    currentJob = job;
+                    this.shiftsToWork = numberOfShifts;
+                    shiftsWorked = 0;
+                    return true;
+                }
+            return false;
+        }
+
+        public bool WorkOneShift()
+        {
+            if (string.IsNullOrEmpty(currentJob))
+                return false;
+            shiftsWorked++;
+            if (shiftsWorked > shiftsToWork)
+            {
+                shiftsToWork = 0;
+                shiftsWorked = 0;
+                currentJob = "";
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
