8780c87061d23b7336c43faa31acd95987a4ce06
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:17:26 +0530

    first commit

---
+++ Chapter8Program3/Chapter8Program2/Shoe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8Program2
+{
+    class Shoe
+    {
+        public Style Style;
+        public string Color;
+    }
+    enum Style
+    {
+        Sneakers,
+        Loafers,
+        Sandals,
+        Flipflops,
+        Wingtips,
+        Clogs,
+    }
+}
---
+++ Chapter8Program3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Chapter8Program3
+{
+    class Program : Duck
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck() {Kind = KindOfDuck.Mallard,Size=17 },
+                new Duck() {Kind = KindOfDuck.Muscovy,Size=18 },
+                new Duck() {Kind = KindOfDuck.Decoy,Size=14 },
+                new Duck() {Kind = KindOfDuck.Muscovy,Size=11 },
+                new Duck() {Kind = KindOfDuck.Mallard,Size=14 },
+                new Duck() {Kind = KindOfDuck.Decoy,Size=13 },
+            };
+            // ducks.Sort();
+            DuckCompareBySize compareOfDucks = new DuckCompareBySize();
+            ducks.Sort(compareOfDucks);
+            PrintDucks(ducks);
+        }
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+            {
+                Console.WriteLine(duck.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
