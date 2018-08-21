03d56adf1b2f0aecb1678f05ab9d61feac3b9100
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:20:06 +0530

    first commit

---
+++ Chapter8Program4/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8Program4
+{
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name
+        {
+            get
+            {
+                return Value.ToString() + " of " + Suit.ToString();
+            }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
---
+++ Chapter8Program4/Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter8Program4
+{
+   public enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts
+    }
+}
