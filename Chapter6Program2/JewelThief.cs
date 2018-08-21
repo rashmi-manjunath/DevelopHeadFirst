5ddfb963d7ac3d503d64b6a80bb10f3d7d009bce
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 16:01:38 +0530

    first commit

---
+++ Chapter6Program2/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6Program2
+{
+    class JewelThief:Locksmith
+    {
+        private Jewels stolenJewels = null;
+        public override void ReturnContents(Jewels safeContents, Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("I'm stealing the contents! " + stolenJewels.Sparkle());
+        }
+    }
+}
---
+++ Chapter6Program2/Safe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6Program2
+{
+    class Safe
+    {
+        private Jewels Contents = new Jewels();
+        private string safeCombination = "12345";
+        public Jewels Open(string combination)
+        {
+            if (combination == safeCombination)
+                return Contents;
+            else
+                return null;
+        }
+        public void PickLock(Locksmith lockpicker)
+        {
+            lockpicker.WriteDownCombination(safeCombination);
+        }
+    }
+}
