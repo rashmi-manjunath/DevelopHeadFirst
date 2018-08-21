a5fe337b23591131201997995a1201f804b158fe
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 15:53:56 +0530

    first commit

---
+++ Chapter4Program3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter4Program3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MenuMaker menu = new MenuMaker() { Randomnizer = new Random()};
+
+            label1.Text = menu.getMenuItem();
+            label2.Text = menu.getMenuItem();
+            label3.Text = menu.getMenuItem();
+            label4.Text = menu.getMenuItem();
+            label5.Text = menu.getMenuItem();
+            label6.Text = menu.getMenuItem();
+            label7.Text = menu.getMenuItem();
+            label8.Text = menu.getMenuItem();
+            label9.Text = menu.getMenuItem();
+
+        }
+    }
+}
---
+++ Chapter4Program3/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4Program3
+{
+    class MenuMaker
+    {
+        public Random Randomnizer = new Random();
+
+        string[] meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
+        string[] condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        string[] breads = { "rye", "white", "wheat", "pumpernickel", "italian bread", "a roll" };
+
+        public string getMenuItem()
+        {
+            string randomMeat = meats[Randomnizer.Next(meats.Length)];
+            string randomCondiment = condiments[Randomnizer.Next(condiments.Length)];
+            string randomBread = breads[Randomnizer.Next(breads.Length)];
+
+            return randomMeat + " with " + randomCondiment + " on " + randomBread;
+        }
+    }
+}
