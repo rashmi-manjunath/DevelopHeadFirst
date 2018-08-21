45ada1b09b22fe7166baf492414ab6e26453e971
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-21 15:38:10 +0530

    first commit

---
+++ Chapter3Program3/Guy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter3Program3
+{
+    public class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int giveCash(int amount)
+        {
+            if (amount > 0 && amount <= Cash)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(Name + "says-I dont have enough cash to give you" + amount);
+                return 0;
+            }
+        }
+
+        public int receiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + "itsn't an amount I'll take", Name + "says");
+                return 0;
+            }
+        }
+    }
+}
