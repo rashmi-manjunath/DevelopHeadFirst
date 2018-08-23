c5a6cf3beee9ab7cb98a0c4e81eed24526cd5d5d
rashmimanjunath <rashmimanjunath79@gmail.com>
2018-08-23 11:04:36 +0530

    first commit

---
+++ Chapter14Program3/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter14Program3
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int ReturnThreeValues(out double half,out int twice)
+        {
+            int value = random.Next(1000);
+            half = ((double)value) / 2;
+            twice = value * 2;
+            return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            double b;
+            int c;
+            a = ReturnThreeValues(out b,out c);
+            Console.WriteLine("value = {0} , half ={1} , double ={2}", a, b, c);
+
+        }
+    }
+}
